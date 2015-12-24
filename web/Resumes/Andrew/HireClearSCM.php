868a6c3c079f272f4aa4cc0030871fe8e76dbec6
Andrew DeFaria <Andrew@DeFaria.com>
2015-12-24 12:17:40 -0800

    Added HireClearSCM.php

---
+++ web/Resumes/Andrew/HireClearSCM.php
@@ -0,0 +1,143 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta name="GENERATOR" content="Mozilla/4.61 [en] (Win98; U) [Netscape]">
+  <title>ClearSCM: Contract with ClearSCM</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="/css/Article.css">
+  <link rel="stylesheet" type="text/css" media="print"  href="/css/Print.css">
+  <link rel="SHORTCUT ICON" href="http://clearscm.com/favicon.ico" type="image/png">
+
+  <!-- Google Analytics
+  <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+    _uacct = "UA-89317-1";
+    urchinTracker ();
+  </script>
+  Google Analytics -->
+
+  <?php
+  include "clearscm.php";
+  menu_css ();
+  ?>
+</head>
+
+<body>
+
+<?php heading ();?>
+
+<div id="page">
+  <div id="content">
+
+  <h3>Contract ClearSCM</h3>
+
+  <p>I know that this position is marked Full Time or Permanent Employment but
+  have you considered filing this position with a consultant instead? Or
+  perhaps utilize a contractor while you look for that perfect employee. I urge
+  to consider ClearSCM's services. I offer my services on a C2C basis and my
+  corporation, ClearSCM, Inc., has been incorporated since 2007 and is in good
+  standing and before that I operated on a 1099 basis for many years. In other
+  words I'm a real consultant and with over 15 years in the consulting business.
+  This enables me to bypass the other levels of recruiting services and
+  consulting firms all of which provide little benefit to your organization
+  except to take a cut of the rate for themselves, and instead offer you my
+  services directly thus saving you money.</p>
+
+  <p>So I ask you to please consider using ClearSCM's services for this 
+  position. For more information contact me directly at:</p>
+
+<style type="text/css">
+.card a:link { 
+  color:		white;
+}
+
+.card a:visited {
+  color:		white;
+}
+
+.card a:hover { 
+  color:		white;
+  background-color:	#004080;
+  text-decoration:	underline;
+}
+
+.card a:active { 
+  color: 		#333;
+}
+
+.name a:link {
+  color:		#fc0;
+}
+
+.name a:visited {
+  color:		#fc0;
+}
+
+.name a:hover {
+  color:		#fc0;
+  background-color:	#333;
+  text-decoration:	underline;
+}
+
+.name a:active {
+  color:		red;
+}
+
+td {
+  font:			Helvetica Arial Serif
+  font-size:		16px;
+}
+</style>
+
+<table bgcolor="#666666" border="0" cellpadding="0" cellspacing="0" width="376" align="center">
+  <tbody>
+    <tr>
+      <td><a class=card href="http://clearscm.com"><img border=0 src="/Logos/ClearSCM.jpg" height="84" width="376"></a></td>
+    </tr>
+    <tr>
+      <td style="margin: 10px;">
+      <table border="0" cellpadding="5" cellspacing="0" width="100%">
+        <tbody>
+          <tr>
+            <td valign="top"><div class=name><a href="http://defaria.com"><b>Andrew DeFaria</b></a></div>
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+              <tbody>
+                <tr>
+                  <td valign="top"><font color=white><b>President</b></font></td>
+                  <td align="right"><div class=card><a href="http://clearscm.com"><b>ClearSCM, Inc.</b></a></div></td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="right"><font color=white size=-1>1676 Hope Drive #1915</font></td>
+                </tr>
+                <tr>
+                  <td><font color=white size=-1><i>The power to see clearly...</i></font></td>
+                  <td align="right"><font color=white size=-1>Santa Clara, California 95054-1721</font></td>
+                </tr>
+                <tr>
+                  <td><font color=white size=-1><i>Professional SCM Consultants</i></font></td>
+                  <td align="right"><font color=white size=-1>Phone: (408) 596-4937</font></td>
+                </tr>
+                <tr>
+                  <td><font color=white><div class=card><a href="http://ClearSCM.com">http://ClearSCM.com</a></div>
+                  <td align="right"><font color=white><div class=card><a href="mailto:Info@ClearSCM.com">Info@ClearSCM.com</a></div></font></td>
+                </tr>
+              </tbody>
+            </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <font color="#ffcc00"></font></td>
+    </tr>
+  </tbody>
+</table>
+
+  <?php copyright ("1988");?>
+
+<script language="JavaScript" src="/JavaScript/Menus.js" type="text/javascript"></script>
+
+</body>
+</html>
