3d827927121cbe2d15bdc07ddecb200eb3e767fa
Andrew DeFaria <Andrew@DeFaria.com>
2021-02-19 11:01:16 -0800

    Changed register handling
    After years of people ignoring the bounce back message I determined that
    it was best to just highlight how to click the fucking link and register
    and not describe anything about MAPS or rather offload that description
    only to those curious enough to actually read the message and find the
    link that gave them more info. This caused me to describe move in
    doc/index.php

--- maps/doc/index.php
+++ maps/doc/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include "site-functions.php";
 include "MAPS.php"
 ?>
@@ -16,7 +16,7 @@ include "MAPS.php"
 </div>
 
 <div class="content">
-  <?php 
+  <?php
   OpenDB ();
   SetContext ($userid);
   NavigationBar ($userid);
@@ -24,7 +24,7 @@ include "MAPS.php"
 
   <h3>What is MAPS?</h3>
 
-  <p>MAPS - which the observant might notice is SPAM spelt backwards -
+  <p>MAPS - which the observant might notice is SPAM spelled backwards -
   works on a simple principal that is commonly used with Instant
   Messenger (IM) clients such as AOL's Instant Messenger or
   Microsoft's Messenger. That is that with most IM clients you need to
@@ -44,42 +44,54 @@ include "MAPS.php"
   filter) which processes your email like so:</p>
 
   <ol>
+    <li>Extract senders email address - no sender address (and no envelope
+    address)? Discard the email</li>
 
-    <li>Extract senders email address - no sender address (and no
-    envelope address)? Discard the email</li>
+    <li>Check to see if the sender is on your white list - if so deliver the
+    mail</li>
 
-    <li>Check to see if the sender is on your null list - if so
-    discard the email</li>
+    <li>Check to see if the sender is on your black list - if so return a
+    message telling the sender that s/he is blocked from emailing you.</li>
 
-    <li>Check to see if the sender is on your black list - if so
-    return a message telling the sender that s/he is blocked from
-    emailing you.</li>
+    <li>Check to see if the sender is on your null list - if so discard the
+    email</li>
 
-    <li>Check to see if the sender is on your white list - if so
-    deliver the mail</li>
-
-    <li>Otherwise send the sender a message with a link for them to
-    quickly register. Also, save their email so it can be delivered
-    when they register</li>
-
+    <li>Otherwise send the sender a bounce back message with a link for them to
+    quickly register. Also, save their email so it can be delivered when they
+    register</li>
   </ol>
 
-  <p>As you can see this algorithm will greatly reduce your
-  spam. Also, it's easy for real people to register. Spammers
-  typically do not read any email returning to them so they never
-  register!</p>
+  <p>As you can see this algorithm will greatly reduce your spam. Also, it's
+  easy for real people to register. Spammers typically do not read any email
+  returning to them so they never register!</p>
+
+  <h3>What to do if you get a bounce back/register email from MAPS?</h3>
+
+  <p>If you receive a bounce back/register email that means you are not yet on
+  my white list. You can register by clicking the link and then typing your
+  name. That's it! You will then be added to my white list and your previous
+  email will be delivered. Also, all future emails from your email address will
+  be automatically delivered. Note, I reserve the right to remove you from my
+  white list and optionally add you to my null or black lists.</p>
+
+  <h3>What to do if you get a black list bounce back email from MAPS?</h3>
+
+  <p>Not much you can do. I've blacklisted you for a reason. I guess you could
+  attempt to contact me another way but chances are I also blocked you phone
+  number from calling or texting me.</p>
+
+  <h3>What to do if you find yourself on my null list?</h3>
+
+  <p>Nothing! It's a null list. Your email would have been silently discarded so
+  how would you know? Note I can't even see it - it was not delivered to me.</p>
 
   <h3>Other topics</h3>
 
   <ul>
-
-  <li><a href="Requirements.php">Requirements</a></li>
-
-  <li><a href="/maps/SignupForm.html">Signup for MAPS</a></li>
-  
-  <li><a href="Using.php">Using MAPS</a></li>
-
-  </ul>  
+    <li><a href="Requirements.php">Requirements</a></li>
+    <li><a href="/maps/SignupForm.html">Signup for MAPS</a></li>
+    <li><a href="Using.php">Using MAPS</a></li>
+  </ul>
 
   <?php copyright (2001);?>
 
