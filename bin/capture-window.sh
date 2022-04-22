6fff5b9c2f216ff68334de853b67427b884935a2
Andrew DeFaria <A.DeFaria@cpanel.net>
2022-04-22 09:50:28 -0700

    Updated screen capture scripts

--- bin/capture-window.sh
+++ bin/capture-window.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-gnome-screenshot -cwf /System/tmp/Screenshots/$(date +%F@%T).png
+timeout=10
+screenshot_directory=/tmp
+
+function yesno {
+  question=$1
+  
+  zenity --question --text "$question" --width=300 --timeout=$timeout 2> /dev/null
+}
+
+mate-screenshot -wc
+
+file=$screenshot_directory/Screenshots/$(date +%F@%T).png
+
+if yesno "Save screenshot as $file?"; then
+  xclip -selection clipboard -t image/png -o > $file
+fi
