af9362f54d786b8b3216100486f9fa3ea6700a0c
Andrew DeFaria <A.DeFaria@cpanel.net>
2023-05-04 10:44:21 -0700

    Various miscellaneous changes

--- bin/updateSongbook.sh
+++ bin/updateSongbook.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 ssh home "cd /opt/songbook && git pull && cd /opt/media && git pull"
-
-# Removed Kent and Mikey so I need to rsync these
-rsync -r /opt/songbook/Kent home:/opt/songbook
-rsync -r /opt/songbook/Mikey home:/opt/songbook
