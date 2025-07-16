99d415142ebc38d0ed0aebf9191c85d022a42821
Andrew DeFaria <Andrew@DeFaria.com>
2025-07-16 16:20:40 -0700

    Updated rsync

--- bin/updateSongbook.sh
+++ bin/updateSongbook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh home "cd /opt/songbook && git pull"
-rsync -rauv /opt/songbook.master/Music/* home:/opt/songbook/Music
+rsync -avzu /opt/songbook.master/Music/* home:/opt/songbook/Music
