cf465009cd7237be5f920a13f238f059a9274cd6
Andrew DeFaria <Andrew@DeFaria.com>
2025-12-07 13:22:33 -0800

    Revert "Fixed problem with %'s in the subject"
    This reverts commit 0dfd9fdd2e33cf7c90c4accd4401bd9dc299454b.

--- bin/announceEmail.pl
+++ bin/announceEmail.pl
@@ -79,7 +79,6 @@ use Mail::IMAPTalk;
 use MIME::Base64;
 use Encode qw(decode);
 use Pod::Usage;
-use URI::Escape;
 use Proc::ProcessTable;
 
 use lib "$FindBin::Bin/../lib";
@@ -250,16 +249,12 @@ sub MonitorMail() {
 
     $subject = decode ('MIME-Header', $subject);
 
-    $log->msg("Subject before cleanup: $subject");
-
-    # URL encode subject for Google Talk
-    $subject = uri_escape($subject);
+    # Google Talk doesn't like #
+    $subject =~ s/\#//g;
 
     # Remove long strings of numbers like order numbers. They are uninteresting
     my $longNumber = 5;
     $subject =~ s/\s+\S*\d{$longNumber,}\S*\s*//g;
-
-    $log->msg("Subject after cleanup: $subject");
 
     # Now speak it!
     my $logmsg = "From $from $subject";
