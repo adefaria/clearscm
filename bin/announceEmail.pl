69903e809fd987e74db6ca9b44387f2b9f151fb9
Andrew DeFaria <Andrew@DeFaria.com>
2025-12-12 12:28:10 -0800

    Improved handling of subject line

--- bin/announceEmail.pl
+++ bin/announceEmail.pl
@@ -79,7 +79,7 @@ use Mail::IMAPTalk;
 use MIME::Base64;
 use Encode qw(decode);
 use Pod::Usage;
-use URI::Escape;
+use URI::Escape qw(uri_escape_utf8);
 use Proc::ProcessTable;
 
 use lib "$FindBin::Bin/../lib";
@@ -250,16 +250,12 @@ sub MonitorMail() {
 
     $subject = decode ('MIME-Header', $subject);
 
-    $log->msg("Subject before cleanup: $subject");
-
     # URL encode subject for Google Talk
-    $subject = uri_escape($subject);
+    $subject = uri_escape_utf8 ($subject);
 
     # Remove long strings of numbers like order numbers. They are uninteresting
     my $longNumber = 5;
     $subject =~ s/\s+\S*\d{$longNumber,}\S*\s*//g;
-
-    $log->msg("Subject after cleanup: $subject");
 
     # Now speak it!
     my $logmsg = "From $from $subject";
