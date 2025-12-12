548074102b79bd08caf055ce3141668757c941e8
Andrew DeFaria <Andrew@DeFaria.com>
2025-12-12 12:28:50 -0800

    Changed home to cloud

--- bin/updateSongbook.sh
+++ bin/updateSongbook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh home "cd /opt/songbook && git pull"
-rsync -avzu --progress /System/NextCloud/andrew/files/SongBook/Music/ home:/opt/songbook/Music
+ssh cloud "cd /opt/songbook && git pull"
+rsync -avzu --progress /System/NextCloud/andrew/files/SongBook/Music/ cloud:/opt/songbook/Music
