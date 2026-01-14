312edaf269995f0464558717437bc32805ac3fb4
Andrew DeFaria <Andrew@DeFaria.com>
2026-01-14 14:28:40 -0800

    fix: Update "Null" button text to "Nulllist" for a submit action.

--- maps/php/MAPS.php
+++ maps/php/MAPS.php
@@ -546,7 +546,7 @@ END;
 
   print <<<END
   <tr>
-    <td align=center colspan=4><input type="submit" name="action" value="Null" onclick="return CheckAtLeast1Checked (document.domains);" /><input type="submit" name="action" value="Reset" onclick="return ClearAll (document.domains);" />
+    <td align=center colspan=4><input type="submit" name="action" value="Nulllist" onclick="return CheckAtLeast1Checked (document.domains);" /><input type="submit" name="action" value="Reset" onclick="return ClearAll (document.domains);" />
     </td>
   </tr>
 <table>
