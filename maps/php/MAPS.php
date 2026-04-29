bc0aa02bdfef3cf624f6eb0262039981de3d28d7
Andrew DeFaria <Andrew@DeFaria.com>
2026-04-29 11:30:43 -0700

    refactor: remove cache-busting timestamp parameters from CSS and JavaScript asset includes

--- maps/php/MAPS.php
+++ maps/php/MAPS.php
@@ -497,13 +497,13 @@ function MAPSHeader()
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Inter:wght@400;500;600;700&family=Outfit:wght@500;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/css/style.css?v=$time">
-  <link rel="stylesheet" type="text/css" href="/maps/css/MAPSStyle.css?v=$time"/>
-  <script language="JavaScript1.2" src="/JavaScript/UIUtils.js?v=$time"
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" type="text/css" href="/maps/css/MAPSStyle.css"/>
+  <script language="JavaScript1.2" src="/JavaScript/UIUtils.js"
    type="text/javascript"></script>
-  <script language="JavaScript1.2" src="/maps/JavaScript/MAPSUtils.js?v=$time"
+  <script language="JavaScript1.2" src="/maps/JavaScript/MAPSUtils.js"
    type="text/javascript"></script>
-  <script language="JavaScript1.2" src="/maps/JavaScript/CheckAddress.js?v=$time"
+  <script language="JavaScript1.2" src="/maps/JavaScript/CheckAddress.js"
    type="text/javascript"></script>
   <script type="text/javascript">
     (function() {
