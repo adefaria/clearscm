4b41e64554d8f3c8248a1511760ec39bc1d95aaf
Andrew DeFaria <Andrew@DeFaria.com>
2026-07-16 12:15:58 -0700

    fix: escape literal parenthesis in JS regex to fix SyntaxError and confirmReportPhishing not defined

--- maps/lib/MAPSWeb.pm
+++ maps/lib/MAPSWeb.pm
@@ -413,7 +413,7 @@ function reportPhishing(sender) {
             }
             
             var dispatchList = stats.dispatch_list || '';
-            msg += "<b>Sent to:</b><br>" + dispatchList.replace(/\(Cc:/, "<br><b>Cc:</b>");
+            msg += "<b>Sent to:</b><br>" + dispatchList.replace(/\\(Cc:/, "<br><b>Cc:</b>");
         } else {
             msg = data.status === 'success' ? data.message.replace(/\n/g, "<br>") : "ERROR: " + data.message;
         }
